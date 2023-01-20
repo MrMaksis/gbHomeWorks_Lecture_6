9561d6f1c79724e4b7a41f7636c12d5649392ed4
mr_maksis <max.morink@yandex.ru>
2023-01-20 15:04:58 +0300

    Task 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,27 @@
-﻿
+﻿#region Задание 1
+void NumMoreNull()
+{
+    string num = " ";
+    int numericValue;
+
+    while(num != "stop")
+    {
+        Console.Write("Введити число/команду: ");
+        num = Console.ReadLine();
+
+        bool isNumber = int.TryParse(num, out numericValue);
+
+        if(isNumber == true){
+            if(Convert.ToInt32(num) > 0){
+                Console.WriteLine($"Число {num} > 0");
+            }
+        }
+        else{
+            Console.WriteLine("Программа завиршила работу!");
+            break;
+        }
+    }
+}
+NumMoreNull(); 
+#endregion
 
